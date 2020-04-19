bc60543c5e18207c87b0c29bb6e22677097e0653
brightonmike <hello@michaelgunner.co.uk>
2020-04-19 12:05:14 +0100

    Change GraphlQL Endpint

--- graphql/apollo-client.js
+++ graphql/apollo-client.js
@@ -4,7 +4,7 @@ import withApollo from 'next-with-apollo';
 import { createHttpLink } from 'apollo-link-http';
 import fetch from 'isomorphic-unfetch';
 
-const GRAPHQL_URL = 'https://code-share-server.brightonmike.now.sh/api/graphql';
+const GRAPHQL_URL = 'https://code-share-server.now.sh/api/graphql';
 
 const link = createHttpLink({
   fetch,
