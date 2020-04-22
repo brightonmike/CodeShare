c4d01d079b55225a32b0fc77be21b1c71261f010
brightonmike <hello@michaelgunner.co.uk>
2020-04-22 11:39:37 +0100

    Fix for local

--- next.config.js
+++ next.config.js
@@ -1,9 +1,10 @@
+const Dotenv = require("dotenv-webpack");
 
 module.exports = {
-    env: {
-        AUTH0_DOMAIN: process.env.AUTH0_DOMAIN || '',
-        AUTH0_REDIRECT: process.env.AUTH0_REDIRECT || '',
-        AUTH0_CLIENT_ID: process.env.AUTH0_CLIENT_ID || '',
-        GRAPHQL_API: process.env.GRAPHQL_API || ''
-    }
-}
+  webpack: (config, { buildId, dev, isServer, defaultLoaders, webpack }) => {
+    // Add the new plugin to the existing webpack plugins
+    config.plugins.push(new Dotenv({ silent: true }));
+
+    return config;
+  }
+};
