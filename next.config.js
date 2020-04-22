877e112e783140328774edcc6f317726944951a3
brightonmike <hello@michaelgunner.co.uk>
2020-04-22 10:43:00 +0100

    Add config file

---
+++ next.config.js
@@ -0,0 +1,9 @@
+
+module.exports = {
+    env: {
+        AUTH0_DOMAIN: process.env.AUTH0_DOMAIN || '',
+        AUTH0_REDIRECT: process.env.AUTH0_REDIRECT || '',
+        AUTH0_CLIENT_ID: process.env.AUTH0_CLIENT_ID || '',
+        GRAPHQL_API: process.env.GRAPHQL_API || ''
+    }
+}
